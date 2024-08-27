60652e0a0dc670eba4db711468e1fe26caeb16b8
Balaji2624 <balajisapkal3062001@gmail.com>
2024-08-27 13:12:12 +0530

    Addtionof two Number

--- src/com/bridezlab/Calculator.java
+++ src/com/bridezlab/Calculator.java
@@ -5,6 +5,9 @@ public class Calculator
     public static void main(String args[])
     {
         System.out.println("Welcome to calculator");
+        int a=10,b=20,c;
+        c=a+b;
+        System.out.println(c);
     }
 
 }
